bc3993ba606616dc9cf45e738152d1cf1bd43823
Natacha De la Rosa <delarosa.natacha@gmail.com>
2018-08-01 10:55:42 -0400

    initial commit

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.4.24;
+
+contract SimpleStorage {
+  uint storedData;
+  string ipfsHash;
+
+  function setHash(string h) public {
+    ipfsHash = h;
+  }
+
+  function getHash(string h) public view returns (string) {
+    return ipfsHash;
+  }
+
+
+  function set(uint x) public {
+    storedData = x;
+  }
+
+  function get() public view returns (uint) {
+    return storedData;
+  }
+
+
+}
